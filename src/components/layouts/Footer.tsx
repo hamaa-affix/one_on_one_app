da93353e999e4e5ae146a2c15e4c6527d728dd3b
hamaa-affix <hamaagive1108@gmail.com>
2021-12-30 12:06:12 +0900

    header, footerスタイル修正

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -2,17 +2,19 @@ import Image from "next/image";
 
 export const Footer = () => {
     return (
-        <footer>
-            <a
-                href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-            Powered by{' '}
-            <span>
-                <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-            </span>
-            </a>
-      </footer>
+        <footer className="flex items-center mt-20">
+            <div className="mx-auto">
+                <a
+                    href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                Powered by{' '}
+                <span>
+                    <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+                </span>
+                </a>
+            </div>
+        </footer>
     );
 }
--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -4,7 +4,11 @@ export const Header = () => {
     return (
         <header className="flex text-center items-center justify-between  w-auto h-24 shadow-xl">
             <div className="flex items-center h-full w-40 bg-red-100">
-                <div className="mx-auto">ロゴ</div>
+                <div className="mx-auto">
+                    <Link href="/">
+                        <a>ロゴ</a>
+                    </Link>
+                </div>
             </div>
             <div className="flex h-full w-40 items-center bg-red-100">
                 <div className="mx-auto">
