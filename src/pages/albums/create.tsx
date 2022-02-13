fa6c5870e850effbf6656e7fd4e3a83ab83d3aa3
hamaa-affix <hamaagive1108@gmail.com>
2022-02-13 10:51:20 +0900

    画像プレビューを複数表示できるように修正

--- src/pages/albums/create.tsx
+++ src/pages/albums/create.tsx
@@ -3,19 +3,20 @@ import Image from "next/image";
 
 //ドロップアンドドロップで投稿できるようにする
 const CreateAlbum: VFC = () => {
-    const [ content, setContent ] = useState<string>();
+    const [ content, setContent ] = useState<Array<string>>([]);
 
     const onChangeFile = (e: ChangeEvent<HTMLInputElement>) => {
         //引数のeventObjectがnullの可能性があるから、このガード説でnullだった時の処理を書かないとエラーになる
         if(!e.target.files) return;
         const file = e.target.files[0];
         const render = new FileReader();
+        console.log(file);
 
         render.onload = e => {
             if(e.target?.result === null) return; 
             if(e.target?.result === undefined) return;
-            console.log(e.target)
-            setContent(e.target?.result.toString());
+
+            setContent([...content, e.target?.result.toString()]);
         }
         //eventで追加したファイルのUrl化、これやらないとプレビュー化でない
         render.readAsDataURL(file);
@@ -23,27 +24,33 @@ const CreateAlbum: VFC = () => {
 
     return (
         <div>
-            <div>アルバム登録</div>
-            <div>
-                <input 
+            <div>写真登録</div>
+            <form action="">
+                <input
                     type="file"
                     name="file"
+                    multiple
                     accept=".png, .jpg, .jpeg, .pdf, .doc"
                     onChange={onChangeFile}
                 /> 
-            </div>
             <div>
                 {
-                    content 
-                    ?  <Image 
-                        src={content}
-                        width={ 500 }
-                        height={ 500 }
-                        alt={ "" }
-                    />
+                    content.length > 0
+                    ? content.map((img, index) => {
+                        return (
+                            <Image 
+                                key={ img }
+                                src={ img }
+                                width={ 250 }
+                                height={ 250 }
+                                alt={ "" }
+                            />
+                        );
+                    })
                     : null
                 }
             </div>
+            </form>
         </div>
     );
 }
